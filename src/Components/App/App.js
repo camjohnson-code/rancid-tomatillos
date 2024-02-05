9175e784a48ed324fe6bc100d355c394eaa3dd62
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-02-04 18:26:51 -0600

    Get initial state and rendering back

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,24 +7,6 @@ import { useState, useEffect } from 'react';
 import AllMovies from '../Movies Display/All-Movies';
 import SingleMoviePage from '../Single Movie Page/SingleMovie';
 
-const dummyMovie = {
-  id: 1,
-  title: 'Fake Movie Title',
-  poster_path:
-    'https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg',
-  backdrop_path:
-    'https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg',
-  release_date: '2019-12-04',
-  overview:
-    'Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!',
-  average_rating: 6,
-  genres: ['Drama'],
-  budget: 63000000,
-  revenue: 100853753,
-  runtime: 139,
-  tagline: "It's a movie!",
-};
-
 const fetchData = (endPoint) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${endPoint}`)
     .then((response) => response.json())
@@ -39,96 +21,109 @@ const fetchData = (endPoint) => {
 };
 
 const App = () => {
+  const dummyMovie = {
+    id: 1,
+    title: 'Fake Movie Title',
+    poster_path:
+      'https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg',
+    backdrop_path:
+      'https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg',
+    release_date: '2019-12-04',
+    overview:
+      'Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!',
+    average_rating: 6,
+    genres: ['Drama'],
+    budget: 63000000,
+    revenue: 100853753,
+    runtime: 139,
+    tagline: "It's a movie!",
+  };
+  
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState('')
   const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(true);
-
 
   useEffect(() => {
     const fetchDataFromApis = async () => {
       try {
         const movieEndpoint = 'movies'; 
-
+        const singleMovieEndpoint = 'movies/movie';
         const [movieResult] = await Promise.all([
-          fetchData(movieEndpoint)
+          fetchData(movieEndpoint),
+          fetchData(singleMovieEndpoint)
         ]);
         setMovies(movieResult.movies);
-        setLoading(false);
+        setMovie(movieResult)
       } catch (error) {
         setError(true);
-        setLoading(false);
       }
     };
 
     fetchDataFromApis();
   }, []); 
   
-  useEffect(() => {
-   if (movies.length > 0) {
-    const popularMovies = movies
+
+  const getRandomMovies = (movies) => {
+    const randomMovies = movies.slice();
+
+    for (let i = randomMovies.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomMovies[i], randomMovies[j]] = [randomMovies[j], randomMovies[i]];
+    }
+    return randomMovies;
+  };
+
+  const popularMovies = movies
     .slice()
     .sort((a, b) => b.average_rating - a.average_rating)
     .slice(0, 14);
-    
-     }
-    setLoading(false);
-     }, [movies]);
-    
-    const getRandomMovies = (movies) => {
-      const randomMovies = movies.slice();
-      
-      for (let i = randomMovies.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [randomMovies[i], randomMovies[j]] = [randomMovies[j], randomMovies[i]];
-      }
-      return randomMovies;
-    };
-    
-  const randomMovies = getRandomMovies(movies);
-  const allMovies = movies
 
-return (
-  <div className='App'>
-    {!movies && <h1 tabIndex='0'>Rancid Tomatillos</h1>}
-    
-    {loading && <p>Loading...</p>}
+  const recommendedMovies = getRandomMovies(movies).slice(0, 14);
 
-    {movies && !loading && (
-      <>
-        <SingleMoviePage
-          title={movies.title}
-          tagline={movies.tagline}
-          overview={movies.overview}
-          releaseDate={movies.release_date}
-          rating={movies.average_rating}
-          genres={movies.genres}
-          runtime={movies.runtime}
-          backdropPath={movies.backdrop_path}
-          setMovie={setMovies}
-        />
-        <Carousel
-          movies={popularMovies}
-          badge='Popular'
-          setMovie={setMovies}
-          dummyMovie={dummyMovie}
-        />
-        <Carousel
-          movies={randomMovies}
-          badge='Recommended'
-          setMovie={setMovies}
-          dummyMovie={dummyMovie}
-        />
-        <AllMovies
-          movies={allMovies}
-          badge='All'
-          setMovie={setMovies}
-          dummyMovie={dummyMovie}
-        />
-      </>
+  const allMovies = movies;
+
+  return (
+    <div className='App'>
+    {!movie && <h1 tabIndex='0'>Rancid Tomatillos</h1>}
+    {movie && (
+      <SingleMoviePage
+        title={movie.title}
+        tagline={movie.tagline}
+        overview={movie.overview}
+        releaseDate={movie.release_date}
+        rating={movie.average_rating}
+        genres={movie.genres}
+        runtime={movie.runtime}
+        backdropPath={movie.backdrop_path}
+        setMovie={setMovie}
+      />
+    )}
+    {!movie && (
+      <Carousel
+        movies={popularMovies}
+        badge='Popular'
+        setMovie={setMovie}
+        dummyMovie={dummyMovie}
+      />
+    )}
+    {!movie && (
+      <Carousel
+        movies={recommendedMovies}
+        badge='Recommended'
+        setMovie={setMovie}
+        dummyMovie={dummyMovie}
+      />
+    )}
+    {!movie && (
+      <AllMovies
+        movies={allMovies}
+        badge='All'
+        setMovie={setMovie}
+        dummyMovie={dummyMovie}
+      />
     )}
   </div>
 );
-
 };
 
 export default App;
