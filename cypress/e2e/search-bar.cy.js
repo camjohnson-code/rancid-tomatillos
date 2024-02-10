0905797b3c66f03066c6c2609df34e5d8c5f1eaf
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-02-10 17:52:24 -0600

    Create sad paths for cypress testing on SearchBar component

--- cypress/e2e/search-bar.cy.js
+++ cypress/e2e/search-bar.cy.js
@@ -1,6 +1,9 @@
 describe('SearchBar functionality', () => {
   beforeEach(() => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+    cy.intercept(
+      'GET',
+      'https://rancid-tomatillos.herokuapp.com/api/v2/movies', 
+    {
       statusCode: 200,
       fixture: '/sample-data.json',
     });
@@ -16,20 +19,40 @@ describe('SearchBar functionality', () => {
     cy.get('input:first-of-type').click().type('sam@turing.io');
     cy.get('input:last-of-type').click().type('123456');
     cy.get('button').click();
-    cy.intercept('GET', 'https://api.example.com/movies',
-    { fixture: '/sample-data.json' }).as('getMovies');
+    cy.intercept(
+      'GET', 
+      'https://rancid-tomatillos.herokuapp.com/api/v2/movies/760204',
+    { fixture: '/the-lair.json' }).as('getMovies');
+
+    cy.intercept(
+      'GET', 
+      'https://rancid-tomatillos.herokuapp.com/api/v2/users/*/ratings', {
+      statusCode: 200,
+      fixture: 'user-ratings.json',
+    }).as('getUserRatings')
   });
 
    it('should display search results when searching for a movie', () => {
     cy.get('.search-input').type('The Lair'); 
-    cy.get('@getMovies'); 
-    cy.get('.movie-cards-container').children().should('have.length', 1); 
+    cy.get('@getMovies');
+    cy.get('.movie-cards-container').children().should('have.length', 1)
   });
  
   it('should redirect to movie details page when clicking on a movie', () => {
     cy.get('.search-input').type('The Lair'); 
     cy.get('@getMovies'); 
     cy.get('.movie-cards-container a').first().click(); 
-    cy.url().should('include', '/movie/760204'); 
+    cy.get('@getUserRatings'); 
+  });
+
+  it('should display a message for no search results', () => {
+    cy.get('.search-input').type('Avatar'); 
+    cy.get('@getMovies');
+    cy.contains('No results found').should('be.visible'); 
+  });
+
+  it('should display a message for empty search', () => {
+    cy.get('.search-input').clear();
+    cy.contains('No results found').should('not.exist');
   });
 });
