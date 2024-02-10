8d1626bdff214021771fd6fd553b4ab63a2cc4c2
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-10 12:16:07 -0700

    Fix bug in log in tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,14 @@
 describe('It should test the application', () => {
   beforeEach(() => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-      statusCode: 200,
-      fixture: '/sample-data.json',
-    });
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
-      statusCode: 201,
-      fixture: '/user-login.json',
-    });
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
-      statusCode: 403,
-      fixture: '/failed-login.json',
-    });
+    cy.intercept(
+      'GET',
+      'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 200,
+        fixture: '/sample-data.json',
+      }
+    ).as('getMovies');
+
     cy.visit('/');
   });
 
@@ -22,25 +19,43 @@ describe('It should test the application', () => {
     });
 
     it('should be able to log in a user', () => {
+      cy.intercept(
+        'POST',
+        'https://rancid-tomatillos.herokuapp.com/api/v2/login',
+        {
+          statusCode: 201,
+          fixture: '/user-login.json',
+        }
+      ).as('loginSuccess');
+
       cy.get('input[name="email"]')
         .type('sam@turing.io')
         .get('input[name="password"]')
         .type('123456')
         .get('button')
-        .click()
-        .url()
-        .should('eq', 'http://localhost:3000/movies');
+        .click();
+      cy.wait('@loginSuccess');
+      cy.url().should('eq', 'http://localhost:3000/movies');
     });
 
     it('should show a message if username or password are incorrect', () => {
+      cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
+        statusCode: 403,
+        fixture: '/failed-login.json',
+      }).as('loginFailed');
+
       cy.get('input[name="email"]')
         .type('sam@turing.io')
         .get('input[name="password"]')
         .type('12345678910')
         .get('button')
-        .click()
-        .get('.error-message')
-        .should('contain', 'Incorrect email or password. Please try again.');
+        .click();
+      cy.wait('@loginFailed');
+      cy.get('.error-message').should(
+        'contain',
+        'Incorrect email or password. Please try again.'
+      );
+      cy.url().should('eq', 'http://localhost:3000/');
     });
   });
 });
